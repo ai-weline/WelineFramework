d61c1facebc3eb809359d79c09b5d59fce4bc083
秋枫雁飞 <1714255949@qq.com>
2021-06-25 17:46:24 +0800

    '兼容PHP8.0下类反射'

--- app/code/Weline/Framework/Manager/ObjectManager.php
+++ app/code/Weline/Framework/Manager/ObjectManager.php
@@ -12,6 +12,7 @@ namespace Weline\Framework\Manager;
 use ReflectionClass;
 use Weline\Framework\App\Exception;
 use Weline\Framework\Cache\CacheInterface;
+use Weline\Framework\Exception\Core;
 use Weline\Framework\Manager\Cache\ObjectCache;
 
 class ObjectManager implements ManagerInterface
@@ -28,7 +29,7 @@ class ObjectManager implements ManagerInterface
 
     private static function getCache()
     {
-        if (! isset(self::$cache)) {
+        if (!isset(self::$cache)) {
             self::$cache = (new ObjectCache())->create();
         }
 
@@ -48,32 +49,29 @@ class ObjectManager implements ManagerInterface
      * @param string $class
      * @param array $arguments
      * @param bool $shared
-     * @throws \ReflectionException
-     * @throws Exception
      * @return mixed|ObjectManager
+     * @throws Exception
+     * @throws \ReflectionException
      */
     public static function getInstance(string $class = '', array $arguments = [], bool $shared = true)
     {
         if (empty($class)) {
             return isset(self::$instance) ? self::$instance : new self();
         }
-        if($class==='\Weline\Theme\Model\WelineThemeFactory'){
-            p();
-        }
         if (isset(self::$instances[$class])) {
             return self::$instances[$class];
         }
 
         // 缓存对象读取 FIXME 需要换回 ！DEV
-        if (! CLI && $shared && ! DEV && $cache_class_object = self::getCache()->get($class)) {
+        if (!CLI && $shared && !DEV && $cache_class_object = self::getCache()->get($class)) {
             self::$instances[$class] = self::initClass($class, $cache_class_object);
             return self::$instances[$class];
         }
 
         // 类名规则处理
-        $new_class               = self::parserClass($class);
-        $arguments               = $arguments ? $arguments : self::getMethodParams($new_class);
-        $new_object              = (new ReflectionClass($new_class))->newInstanceArgs($arguments);
+        $new_class = self::parserClass($class);
+        $arguments = $arguments ? $arguments : self::getMethodParams($new_class);
+        $new_object = (new ReflectionClass($new_class))->newInstanceArgs($arguments);
         self::$instances[$class] = self::initClass($class, $new_object);
 
         // 缓存对象
@@ -85,14 +83,14 @@ class ObjectManager implements ManagerInterface
     public static function parserClass(string $class)
     {
         // 拦截器处理
-        $new_class   = $class;
+        $new_class = $class;
         $interceptor = $class . '\\Interceptor';
 
         if (class_exists($interceptor)) {
             $new_class = $interceptor;
         }
         // 工厂类处理 工厂类不存在时还原类
-        if (! class_exists($class)) {
+        if (!class_exists($class)) {
             $new_class = self::initFactoryClass($class);
         }
 
@@ -118,57 +116,57 @@ class ObjectManager implements ManagerInterface
 
     /**
      * @Desc         | 创建实例并运行
-     * @param $className
+     * @param $typeName
      * @param string $methodName
      * @param array $params
-     * @throws \ReflectionException
      * @return mixed
+     * @throws \ReflectionException
      */
-    public static function make($className, $methodName = '__construct', $params = [])
+    public static function make($typeName, $methodName = '__construct', $params = [])
     {
         // 拦截器处理
-        $new_class = self::parserClass($className);
+        $new_class = self::parserClass($typeName);
         if ('__construct' === $methodName) {
 //            throw  new Exception(__('无法通过make方式执行__construct函数！'));
-            if (isset(self::$instances[$className])) {
-                return self::$instances[$className];
+            if (isset(self::$instances[$typeName])) {
+                return self::$instances[$typeName];
             }
             // 如果是初始化函数则返回一个初始化后的对象
             // 缓存对象读取
-            if (! DEV && $cache_class_object = self::getCache()->get($new_class)) {
-                self::$instances[$className] = self::initClass($className, $cache_class_object);
+            if (!DEV && $cache_class_object = self::getCache()->get($new_class)) {
+                self::$instances[$typeName] = self::initClass($typeName, $cache_class_object);
 
-                return self::$instances[$className];
+                return self::$instances[$typeName];
             }
-            $instance                    = (new ReflectionClass($new_class))->newInstanceArgs($params);
-            self::$instances[$className] = $instance;
-            self::getCache()->set($className, $instance);
+            $instance = (new ReflectionClass($new_class))->newInstanceArgs($params);
+            self::$instances[$typeName] = $instance;
+            self::getCache()->set($typeName, $instance);
 
-            return self::$instances[$className];
+            return self::$instances[$typeName];
         }
         // 如果不是则实例化后立即执行该函数
         // 获取该方法所需要依赖注入的参数
-        $paramArr = self::getMethodParams($className, $methodName);
+        $paramArr = self::getMethodParams($typeName, $methodName);
         // 获取类的实例
-        self::$instances[$className] = self::getInstance($className);
+        self::$instances[$typeName] = self::getInstance($typeName);
 
-        return self::$instances[$className]->{$methodName}(...array_merge($paramArr, $params));
+        return self::$instances[$typeName]->{$methodName}(...array_merge($paramArr, $params));
     }
 
     /**
      * @Desc         | 获取方法参数,插件实现
-     * @param $className
+     * @param $typeName
      * @param string $methodsName
-     * @throws Exception
      * @return array
+     * @throws Exception
      */
-    protected static function getMethodParams($className, $methodsName = '__construct')
+    protected static function getMethodParams($typeName, $methodsName = '__construct')
     {
         // 通过反射获得该类
         try {
-            $class = new ReflectionClass($className);
+            $class = new ReflectionClass($typeName);
         } catch (\ReflectionException $e) {
-            throw new Exception(__('无法实例化该类：%1，错误：%2', [$className, $e->getMessage()]), $e);
+            throw new Exception("无法实例化该类：{$typeName}，错误：{$e->getMessage()}", $e);
         }
 
         $paramArr = []; // 记录参数，和参数类型（例如：class,string等）
@@ -179,27 +177,28 @@ class ObjectManager implements ManagerInterface
             try {
                 $construct = $class->getMethod($methodsName);
             } catch (\ReflectionException $e) {
-                throw new Exception(__('无法获得对象方法：%1，错误：%2', [$methodsName, $e->getMessage()]), $e);
+                throw new Exception("无法获得对象方法：{$methodsName}，错误：{$e->getMessage()}", $e);
             }
             // 判断构造函数是否有参数
             $params = $construct->getParameters();
 
             if (count($params) > 0) {
                 // 判断参数类型
-                foreach ($params as $key => $param) {
-                    if ($paramClass = $param->getClass()) {
-                        if (isset(self::$instances[$paramClass->getName()])) {
-                            $paramArr[] = self::$instances[$paramClass->getName()];
+                foreach ($params as $param) {
+                    $paramType = $param->getType();
+                    if ($paramType && class_exists($paramType->getName())) {
+                        // 获得参数类型名称
+                        $paramTypeName = $paramType->getName();
+                        if (isset(self::$instances[$paramTypeName])) {
+                            $paramArr[] = self::$instances[$paramTypeName];
                         } else {
-                            // 获得参数类型名称
-                            $paramClassName = $paramClass->getName();
                             // 获得参数类型
-                            $args = self::getMethodParams($paramClassName);
+                            $args = self::getMethodParams($paramTypeName);
                             // 实例化时执行自定义__init方法
                             try {
-                                $newObj = (new ReflectionClass(self::parserClass($paramClass->getName())))->newInstanceArgs($args);
+                                $newObj = (new ReflectionClass(self::parserClass($paramType->getName())))->newInstanceArgs($args);
                             } catch (\ReflectionException $e) {
-                                throw new Exception(__('无法实例化该类：%1，错误：%2', [$paramClass->getName(), $e->getMessage()]), $e);
+                                throw new Exception("无法实例化该类：{$paramType->getName()}，错误：{$e->getMessage()}", $e);
                             }
                             if (method_exists($newObj, '__init')) {
                                 $newObj->__init();
@@ -210,7 +209,7 @@ class ObjectManager implements ManagerInterface
                         try {
                             $paramArr[] = $param->getDefaultValue();
                         } catch (\ReflectionException $e) {
-                            throw new Exception(__('错误的参数：%1', [$e->getMessage()]), $e);
+                            throw new Exception("错误的参数!，错误：{$e->getMessage()}", $e);
                         }
                     }
                 }
@@ -226,8 +225,8 @@ class ObjectManager implements ManagerInterface
      * 参数区：
      *
      * @param $class
-     * @throws \ReflectionException
      * @return ReflectionClass
+     * @throws \ReflectionException
      */
     protected function getReflectionClass($class): ReflectionClass
     {
