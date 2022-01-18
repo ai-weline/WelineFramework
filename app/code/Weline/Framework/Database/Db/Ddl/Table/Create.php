d4f88d0d9c73e739a051900ff27a23410f6c785b
秋枫雁飞 <1714255949@qq.com>
2022-01-18 14:22:10 +0800

    处理创建表失败时展示sql数据

--- app/code/Weline/Framework/Database/Db/Ddl/Table/Create.php
+++ app/code/Weline/Framework/Database/Db/Ddl/Table/Create.php
@@ -182,7 +182,7 @@ createSQL;
         try {
             $result = $this->connection->query($sql);
         } catch (\Exception $exception) {
-            throw new Exception(__('创建表失败，SQL：%1 '.PHP_EOL.PHP_EOL.'ERROR：%2',[$sql,$exception->getMessage()]));
+            throw new Exception(__('创建表失败，'.PHP_EOL.PHP_EOL.'SQL：%1 '.PHP_EOL.PHP_EOL.'ERROR：%2',[$sql,$exception->getMessage()]));
         }
         return $result;
     }
