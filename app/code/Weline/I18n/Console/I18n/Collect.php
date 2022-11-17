bc91b80090e42d60f7ac8dc5d7f3a6876b2afb11
秋枫雁飞 <1714255949@qq.com>
2022-11-18 00:08:00 +0800

    语言包收集命令

--- app/code/Weline/I18n/Console/I18n/Collect.php
+++ app/code/Weline/I18n/Console/I18n/Collect.php
@@ -14,17 +14,25 @@ namespace Weline\I18n\Console\I18n;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use Weline\Framework\App\Env;
+use Weline\Framework\App\Exception;
 use Weline\Framework\Output\Cli\Printing;
+use Weline\I18n\Model\I18n;
 
 class Collect implements \Weline\Framework\Console\CommandInterface
 {
-    private Client $client;
     private Printing $printing;
+    /**
+     * @var \Weline\I18n\Model\I18n
+     */
+    private I18n $i18n;
 
-    public function __construct(Client $client, Printing $printing)
+    public function __construct(
+        I18n     $i18n,
+        Printing $printing
+    )
     {
-        $this->client   = $client;
         $this->printing = $printing;
+        $this->i18n     = $i18n;
     }
 
     /**
@@ -33,7 +41,12 @@ class Collect implements \Weline\Framework\Console\CommandInterface
     public function execute(array $args = [], array $data = [])
     {
         # 设置语言翻译收集配置
-
+        try {
+            $this->i18n->convertToLanguageFile();
+            $this->printing->success(__('语言包收集成功！'));
+        } catch (Exception $e) {
+            $this->printing->error(__('语言包收集失败：%1', $e->getMessage()));
+        }
         # 查找所有已激活模块的模板文件，进行模板生成
     }
 
