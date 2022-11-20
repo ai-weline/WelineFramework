dffbfacea0e3f06709a1075d17a68cca6aea0aef
秋枫雁飞 <1714255949@qq.com>
2022-11-21 00:14:25 +0800

    添加sort排序器缓存

--- app/code/Weline/Component/Block/Tool/SortBar.php
+++ app/code/Weline/Component/Block/Tool/SortBar.php
@@ -17,83 +17,86 @@ use Weline\Framework\System\Security\Encrypt;
 
 class SortBar extends \Weline\Framework\View\Block
 {
+    public const default_sorts = [
+        'default' => [
+            'field' => 'create_time',
+            'sort'  => 'desc'
+        ],
+        'lasted'  => [
+            'field' => 'create_time',
+            'sort'  => 'desc'
+        ],
+        'update'  => [
+            'field' => 'update_time',
+            'sort'  => 'desc'
+        ]
+    ];
     protected string $_template = 'Weline_Component::tool/sort-bar.phtml';
 
     public function __init()
     {
         parent::__init();
-        $action        = $this->request->getUrlBuilder()->getUrl();
-        $action_sorts  = $this->getData('sorts');#lasted=>create_time:desc
-        $up_icon       = $this->getData('up_icon') ?: '<i class="mdi mdi-arrow-up"></i>';
-        $down_icon     = $this->getData('down_icon') ?: '<i class="mdi mdi-arrow-down"></i>';
-        $default_sorts = [
-            'default' => 'create_time:desc',
-            'lasted'  => 'create_time:desc',#create_time:desc
-            'update'  => 'update_time:desc',
-        ];
-        $sorts         = $default_sorts;
-        if (is_string($action_sorts)) {
-            $action_sorts = explode(',', $action_sorts);
-            foreach ($action_sorts as $key => $action_sort) {
-                unset($action_sorts[$key]);
-                $action_sort = explode('=>', $action_sort);
-                if (count($action_sort) === 2) {
-                    $action_sorts[$action_sort[0]] = $action_sort[1];
+        $action = $this->request->getUrlBuilder()->getUrl();
+        $filter = $this->getData('sorter');# 排序器名字，用于读取映射缓存
+        if (empty($filter)) {
+            throw new \Exception(__('排序器ID不能为空：%1', htmlentities('<block class="">')));
+        }
+        # 查看排序器缓存
+        $sorts = $this->_cache->get($filter) ?: [];
+        if (!$sorts) {
+            $action_sorts       = $this->getData('sorts');#lasted=>create_time:desc
+            $up_icon            = $this->getData('up_icon') ? '<i class="' . $this->getData('up_icon') . '"></i>' : '<i class="mdi mdi-arrow-up"></i>';
+            $down_icon          = $this->getData('down_icon') ? '<i class="' . $this->getData('down_icon') . '"></i>' : '<i class="mdi mdi-arrow-down"></i>';
+            $sorts['up_icon']   = $up_icon;
+            $sorts['down_icon'] = $down_icon;
+            $sorts['data']      = self::default_sorts;
+            if ($action_sorts) {
+                $action_sorts = explode(',', $action_sorts);
+                foreach ($action_sorts as $key => $action_sort) {
+                    unset($action_sorts[$key]);
+                    $action_sort = explode('=>', $action_sort);
+                    if (count($action_sort) === 2) {
+                        $action_sort_value             = explode(':', $action_sort[1]);
+                        $action_sorts[$action_sort[0]] = [
+                            'field' => $action_sort_value[0],
+                            'sort'  => $action_sort_value[1] ?? 'desc',
+                        ];
+                    }
+                }
+                if ($action_sorts) {
+                    $sorts['data'] = array_merge($sorts['data'], $action_sorts);
                 }
             }
-            if ($action_sorts) {
-                $sorts = array_merge($default_sorts, $action_sorts);
-            }
-        } elseif (is_array($action_sorts)) {
-            $sorts = array_merge($default_sorts, $action_sorts);
+            $this->_cache->set($filter, $sorts);
         }
 
         # params排序反转
-        $params     = $this->request->getParams();
-        $target     = false;
-        foreach ($sorts as $key => $sort) {
-            if (is_array($sort)) {
-                break;
+        $params    = $this->request->getParams();
+        $sort_data = [];
+        foreach ($sorts['data'] as $key => $sort) {
+            $icon      = $sorts['down_icon'];
+            $target = isset($params[$key]);
+            if ($target) {
+                $sort['sort'] = $params[$key];
             }
-            $icon       = $down_icon;
-            $sort = str_replace('%3A', ':', $sort);
-            $str_len = strlen($sort);
-            if (isset($params[$key])) {
-                $sort = str_replace('%3A', ':', $params[$key]);
-                $str_len = strlen($sort);
-                $target = true;
-                if (str_ends_with(strtolower($sort), ':desc')) {
-                    $sort = substr($sort, 0, $str_len - 5) . ':asc';
-                    $icon = $up_icon;
-                } elseif (str_ends_with(strtolower($sort), ':asc')) {
-                    $sort = substr($sort, 0, $str_len - 4) . ':desc';
-                } else {
-                    $sort = $sort . ':desc';
-                }
-            } elseif ($pos = strrpos($sort, ':')) {
-                if ($pos === ($str_len - 1)) {
-                    $sort = $sort . ':desc';
-                } else {
-                    if (str_ends_with(strtolower($sort), ':asc')) {
-                        $icon = $up_icon;
-                    }
-                }
-            } else {
-                $sort = $sort . ':desc';
+            switch (strtolower($sort['sort'])) {
+                case 'desc':
+                    $sort['sort'] = 'asc';
+                    break;
+                case 'asc':
+                    $sort['sort'] = 'desc';
+                    $icon         = $sorts['up_icon'];
+                    break;
             }
-            $sorts[$key] = [
-                'href'   => $this->request->getUrlBuilder()->extractedUrl([$key => $sort], true),
+            $sort_data[$key] = [
+                'href'   => $this->request->getUrlBuilder()->extractedUrl([$key => $sort['sort']], true),
                 'name'   => __($key),
-                'tag'    => $sort,
                 'icon'   => $icon,
+                'sort'   => $sort['sort'],
                 'target' => $target,
             ];
         }
-//        d($params);
-//        dd($sorts);
-
-        $this->assign('sorts', $sorts);
-        $this->assign('id', md5($action) . Encrypt::get_rand_number_code());
+        $this->assign('sorts', $sort_data);
         $this->assign('action', $action);
     }
 }
