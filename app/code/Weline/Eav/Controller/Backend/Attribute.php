05f4fc868dcc1fd6a5fccbe78f61930998c3194f
秋枫雁飞 <1714255949@qq.com>
2023-04-10 23:33:28 +0800

    完成属性管理，需要继续完成属性类型管理

--- app/code/Weline/Eav/Controller/Backend/Attribute.php
+++ app/code/Weline/Eav/Controller/Backend/Attribute.php
@@ -39,29 +39,94 @@ class Attribute extends \Weline\Framework\App\Controller\BackendController
         return $this->fetch();
     }
 
-    function edit()
-    {
-        $code      = $this->request->getGet('code');
-        $attribute = $this->eavAttribute->loadByCode($code);
-        $this->assign('attribute', $attribute);
-        $this->init_form();
-        return $this->fetch('form');
-    }
-
     function add()
     {
+        if ($this->request->isPost()) {
+            try {
+                $group_id = $this->request->getPost('group_id');
+                $set_id   = $this->request->getPost('set_id');
+                /**@var Group $groupModel */
+                $groupModel = ObjectManager::getInstance(Group::class);
+                $group      = $groupModel->where('set_id', $set_id)->where('group_id', $group_id)->find()->fetchOrigin();
+                if (!$group) {
+                    $this->getMessageManager()->addWarning(__('分组不在所选属性集内！'));
+                    $this->session->setData('attribute', $this->request->getPost());
+                    $this->redirect($this->_url->getCurrentUrl());
+                }
+                $this->eavAttribute->setData($this->request->getPost())
+                                   ->save(true);
+                $this->getMessageManager()->addSuccess(__('添加成功！'));
+                $this->session->delete('attribute');
+            } catch (\Exception $exception) {
+                $this->getMessageManager()->addWarning(__('添加异常！'));
+                if (DEBUG || DEV) $this->getMessageManager()->addException($exception);
+            }
+            $this->redirect($this->_url->getBackendUrl('*/backend/attribute/edit', ['code' => $this->request->getPost('code')]));
+        }
+        if ($data = $this->session->getData('attribute')) {
+            $this->assign('attribute', $data);
+        }
         $this->init_form();
         return $this->fetch('form');
     }
 
-    protected function init_form(){
-        /**@var \Weline\Eav\Model\EavAttribute\Type $typeModel*/
+    function edit()
+    {
+        if ($this->request->isPost()) {
+            try {
+                $group_id = $this->request->getPost('group_id',0);
+                $set_id   = $this->request->getPost('set_id',0);
+                /**@var Group $groupModel */
+                $groupModel = ObjectManager::getInstance(Group::class);
+                $group      = $groupModel->where('set_id', $set_id)->where('group_id', $group_id)->find()->fetchOrigin();
+                if (!$group) {
+                    $this->getMessageManager()->addWarning(__('分组不在所选属性集内！'));
+                    $this->session->setData('attribute', $this->request->getPost());
+                    $this->redirect($this->_url->getCurrentUrl());
+                }
+                $this->eavAttribute->setData($this->request->getPost())
+                                   ->save(true);
+                $this->getMessageManager()->addSuccess(__('修改成功！'));
+                $this->session->delete('attribute');
+            } catch (\Exception $exception) {
+                $this->getMessageManager()->addWarning(__('修改异常！'));
+                if (DEBUG || DEV) $this->getMessageManager()->addException($exception);
+            }
+            $this->redirect($this->_url->getBackendUrl('*/backend/attribute/edit', ['code' => $this->request->getPost('code')]));
+        }
+        $this->init_form();
+        return $this->fetch('form');
+    }
+
+    function getDelete()
+    {
+        if ($code = $this->request->getGet('code')) {
+            $this->eavAttribute->load('code', $code)->delete();
+            $this->getMessageManager()->addSuccess(__('删除成功！'));
+        } else {
+            $this->getMessageManager()->addError(__('找不到要操作的代码！'));
+        }
+        $this->redirect($this->_url->getBackendUrl('*/backend/attribute'));
+    }
+
+    protected function init_form()
+    {
+        if ($code = $this->request->getGet('code')) {
+            $this->assign('attribute', $this->eavAttribute->load('code',$code));
+        }
+        /**@var \Weline\Eav\Model\EavAttribute\Type $typeModel */
         $typeModel = ObjectManager::getInstance(EavAttribute\Type::class);
-        $types = $typeModel->select()->fetchOrigin();
+        $types     = $typeModel->select()->fetchOrigin();
         $this->assign('types', $types);
-        /**@var Group $grouModel*/
+        /**@var Group $grouModel */
         $groupModel = ObjectManager::getInstance(Group::class);
-        $groups = $groupModel->select()->fetchOrigin();
+        $groups     = $groupModel->select()->fetchOrigin();
         $this->assign('groups', $groups);
+        /**@var EavAttribute\Set $setModel */
+        $setModel = ObjectManager::getInstance(EavAttribute\Set::class);
+        $sets     = $setModel->select()->fetchOrigin();
+        $this->assign('sets', $sets);
+        // 链接
+        $this->assign('action', $this->_url->getCurrentUrl());
     }
 }
